03c83e0dfc7406cc317d0eace44947902493f64c
jingling <crazyjingling@126.com>
2019-05-17 15:45:29 +0800

    rm accessLog param logType

--- app/middleware/accessLog.js
+++ app/middleware/accessLog.js
@@ -1,6 +1,6 @@
 const RainLog = require('rain-log');
 
 module.exports = opts => {
-  const requestLogger = new RainLog({name: opts.name || this.app.name, logType: 'file'});
+  const requestLogger = new RainLog({name: opts.name || this.app.name});
   return requestLogger.egg_access_log(opts)
 };
